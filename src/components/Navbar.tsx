000159dc20220812ff11124d3f74344287f56309
Shoeb891 <shoebm891@gmail.com>
2025-09-03 19:18:40 +0530

    Nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,8 +19,7 @@ function Navbar() {
                 <div className="flex items-center justify-around w-full max-w-8xl mx-auto">
                     <Link href="/" className="flex-shrink-0">
                        <Image
-                                 className="dark:invert"
-                                 src="/next.svg"
+                                 src="/2.svg"
                                  alt="Next.js logo"
                                  width={180}
                                  height={38}
